9ca64496a59479fcb58ce59290409d698ff4a2bb
dot-json <jason.97.liew@gmail.com>
2017-05-29 23:03:06 +1000

    add fallouts & resolutions pages

--- js/appafom.js
+++ js/appafom.js
@@ -1,37 +1,92 @@
 var app = angular.module("myApp", ["ngRoute"]);
 
 app.config(function ($routeProvider, $locationProvider) {
+
 	$locationProvider.hashPrefix("!");
 
 	// config routes
 	$routeProvider
-		.when("/home", {
+		.when("/", {
 			templateUrl: "templates/home.html"
 		})
-		.when("/detected", {
-			templateUrl: "templates/detected.html"
+		.when("/fallouts", {
+			controller	: "falloutsCtrl",
+			templateUrl	: "templates/fallouts.html"
 		})
-		.when("/resolved", {
-			templateUrl: "templates/resolved.html"
+		.when("/resolutions", {
+			controller	: "resolutionsCtrl",
+			templateUrl	: "templates/resolutions.html"
 		})
 		.when("/statistics", {
 			templateUrl: "templates/statistics.html"
 		})
+		.when("/404", {
+			templateUrl: "templates/404.html"
+		})
 		.otherwise ({ 
-			redirectTo: "/home" 
+			redirectTo: "404" 
 		});
 });
 
 app.controller("mainCtrl", function ($scope) {
-	// $http.get("file-data.json")
- //    .then(function(response) {
- //        // this callback will be called asynchronously when the response is available
- //        $scope.units = response.data;
- //    }, function(response) {
- //        // called asynchronously if an error occurs or server returns response with an error status.
- //    });
+
 });
 
-app.controller("detectedCtrl", function ($scope, $routeParams) {
-	// agkl;
+app.controller("falloutsCtrl", function ($scope, $http) {
+	
+	// fallout data init
+	$scope.fallouts = [];
+
+	// table sort data init
+	$scope.sortType	= "id"; 	// set default sort type
+	$scope.sortReverse = false;  	// set default sort order
+	$scope.searchId = "";	 	// set default search/filter term
+
+	// get fallout data from herokuapp api
+	$http.get("https://comptel-api.herokuapp.com/api/fallouts")
+	.then(function(response) { 
+		// async success
+		$scope.fallouts = response.data.docs;
+	}, function(response) { 
+		// async error
+	});
+});
+
+app.controller("resolutionsCtrl", function ($scope, $http) {
+	
+	// fallout data init
+	$scope.resolutions = [];
+
+	// table sort data init
+	$scope.sortType	= "id"; 	// set default sort type
+	$scope.sortReverse = false;  	// set default sort order
+	$scope.searchId = "";	 	// set default search/filter term
+
+	// get fallout data from herokuapp api
+	$http.get("https://comptel-api.herokuapp.com/api/resolutions")
+	.then(function(response) { 
+		// async success
+		$scope.resolutions = response.data.docs;
+	}, function(response) { 
+		// async error
+	});
+});
+
+app.directive("ngDetails", function ($parse, $http) {
+	var linkFunction = function (scope, element, attributes) {
+		scope.query = "https://comptel-api.herokuapp.com/api/details/" + attributes.ngDetails;
+
+		$http.get(scope.query)
+		.then(function(response) { 
+			// async success
+			scope.details = response.data;
+		}, function(response) { 
+			// async error
+		});
+	};
+	return {
+		restrict: "A",
+		link: linkFunction,
+		template: "<ul data-ng-repeat='d in details'><li><strong>Name: </strong>{{ d.property_name }}</li><li><strong>Property: </strong>{{ d.property_value }}</li></ul>"
+	};
 });
