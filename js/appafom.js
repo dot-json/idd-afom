f979dc3de36fe471bf3cdfbcfc98b66ccffd11b1
dot-json <jason.97.liew@gmail.com>
2017-05-30 22:31:17 +1000

    fallouts bargraph

--- js/appafom.js
+++ js/appafom.js
@@ -1,10 +1,11 @@
-var app = angular.module("myApp", ["ngRoute"]);
+// the application
+var app = angular.module("myApp", ["ngRoute", "chart.js"]);
 
+// config views
 app.config(function ($routeProvider, $locationProvider) {
-
 	$locationProvider.hashPrefix("!");
 
-	// config routes
+	// routes
 	$routeProvider
 		.when("/", {
 			templateUrl: "templates/home.html"
@@ -23,44 +24,101 @@ app.config(function ($routeProvider, $locationProvider) {
 		.when("/404", {
 			templateUrl: "templates/404.html"
 		})
-		.otherwise ({ 
+		.otherwise ({ // default
 			redirectTo: "404" 
 		});
+});
+
+app.factory("FalloutData", function($http) {
+	var service = {};
+
+	service.getData = function(query) {
+		return $http.get(query);
+	};
+
+	return service;
+});
+
+app.factory("GraphData", function() {
+	var service = {};
+
+	service.getFalloutsVsTime = function(data) {
+		var falloutArray = [];
+
+		for (var i = 0; i < 24; i++) {
+			var count = 0;
+
+			angular.forEach(data, function(value, key) {
+				var hour = value.creation_date.substring(11, 13);
+				if (i == hour) {
+					count++;
+				} 
+			});
+
+			falloutArray.push(count);
+		}
+
+		return falloutArray;
+	}
+
+	service.get24h = function(data) {
+		var hours = [];
+
+		for (var i = 0; i < 24; i++) {
+			if (i == 0) {
+				hours.push("12am");
+			} else if (i < 12) {
+				hours.push(i + "am");
+			} else if (i == 12) {
+				hours.push("12pm");
+			} else {
+				hours.push((i - 12) + "pm");
+			}
+		}
+
+		return hours;
+	}
+
+	return service;
 });
 
 app.controller("mainCtrl", function ($scope) {
 
 });
 
-app.controller("falloutsCtrl", function ($scope, $http) {
-	
-	// fallout data init
-	$scope.fallouts = [];
-
+// controller for fallouts view
+app.controller("falloutsCtrl", function ($scope, FalloutData, GraphData) {
 	// table sort data init
-	$scope.sortType	= "id"; 	// set default sort type
-	$scope.sortReverse = false;  	// set default sort order
-	$scope.searchId = "";	 	// set default search/filter term
+	$scope.sortType	= "id"; 		// set default sort type
+	$scope.sortReverse = false;		// set default sort order
+	$scope.searchId = "";	 		// set default search/filter term
 
-	// get fallout data from herokuapp api
-	$http.get("https://comptel-api.herokuapp.com/api/fallouts")
-	.then(function(response) { 
+	// chart data
+	$scope.falloutsPerDay = [];
+	
+	FalloutData.getData("https://comptel-api.herokuapp.com/api/fallouts")
+	.then(function(response) {
 		// async success
 		$scope.fallouts = response.data.docs;
+		console.log(GraphData.getFalloutsVsTime($scope.fallouts));
+
+		$scope.labels = GraphData.get24h();
+		$scope.data = [GraphData.getFalloutsVsTime($scope.fallouts)];
+		$scope.series = ["Fallouts at this time"];
 	}, function(response) { 
 		// async error
 	});
+
+	//bullshet
+	
 });
 
+// controller for resolutions view
 app.controller("resolutionsCtrl", function ($scope, $http) {
-	
-	// fallout data init
-	$scope.resolutions = [];
-
 	// table sort data init
-	$scope.sortType	= "id"; 	// set default sort type
+	$scope.sortType	= "id"; 		// set default sort type
 	$scope.sortReverse = false;  	// set default sort order
-	$scope.searchId = "";	 	// set default search/filter term
+	$scope.searchId = "";	 		// set default search/filter term
 
 	// get fallout data from herokuapp api
 	$http.get("https://comptel-api.herokuapp.com/api/resolutions")
@@ -70,10 +128,14 @@ app.controller("resolutionsCtrl", function ($scope, $http) {
 	}, function(response) { 
 		// async error
 	});
+
+
 });
 
+// directive for listing additional details based on fallout id
 app.directive("ngDetails", function ($parse, $http) {
 	var linkFunction = function (scope, element, attributes) {
+		// create GET query for the api
 		scope.query = "https://comptel-api.herokuapp.com/api/details/" + attributes.ngDetails;
 
 		$http.get(scope.query)
@@ -89,4 +151,12 @@ app.directive("ngDetails", function ($parse, $http) {
 		link: linkFunction,
 		template: "<ul data-ng-repeat='d in details'><li><strong>Name: </strong>{{ d.property_name }}</li><li><strong>Property: </strong>{{ d.property_value }}</li></ul>"
 	};
+});
+
+// TODO
+// formats datetime as "yyyy-mm-dd hh-mm-ss"
+app.filter('split', function() {
+	return function(input, splitChar, splitIndex) {
+		return input.split(splitChar)[splitIndex];
+	}
 });
